7d5b67f0fd9059b64ee101b4fc6f1efbe8c66e2b
liuwq <3304284977@qq.com>
2021-08-24 14:08:35 +0800

    1.搭建环境

---
+++ yingxue_service/yingxue_liuwq/src/main/java/com/baizhi/YingxueLiuwqApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YingxueLiuwqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YingxueLiuwqApplication.class, args);
+    }
+
+}
